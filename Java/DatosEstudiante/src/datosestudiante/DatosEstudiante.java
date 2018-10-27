d4a13ec1a8e5adb59719b9167d758db2727837c2
wilmer quijano <tuescuelaenlinea@hotmail.com>
2018-10-26 21:30:30 -0500

    Iniciando

---
+++ Java/DatosEstudiante/src/datosestudiante/DatosEstudiante.java
@@ -0,0 +1,25 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package datosestudiante;
+
+/**
+ *
+ * @author Next University
+ */
+public class DatosEstudiante {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+        System.out.println("Mi nombre es Juan Martínez");
+        System.out.println("Soy de España");
+        System.out.println("Tengo 20 años");
+        
+    }
+    
+}
---
+++ Java/JavaNetbeans/src/nextu/javanetbeans/JavaNetbeans.java
@@ -0,0 +1,15 @@
+package nextu.javanetbeans;
+
+/**
+ *
+ * @author Estudiante Next University
+ */
+public class JavaNetbeans {
+
+    public static void main(String[] args) {
+        System.out.println("¡Bienvenido al entorno de desarrollo Netbeans!");
+                
+    }
+    
+}
+    
